1c20862f3f81a1d2d1decd6b21f69b8b23bb8ea7
R. Bryan Smith <Rodney.Bryan@gmail.com>
2014-08-14 14:46:23 -0400

    Fork to keep script working with latest API
    Various changes to keep script working on a Windows machine with latest
    gmusicapi

--- musicsync.py
+++ musicsync.py
@@ -1,7 +1,7 @@
 """
     musicsync.py
 
-    Provides a utility class around the Google Music API that allows for easy synching of playlists.
+    Provides a utility class around the Google Music API that allows for easy syncing of playlists.
     Currently it will look at all the files already in the playlist and:
      Upload any missing files (and add them to the playlist)
      Add any files that are already uploaded but not in the online playlist
@@ -32,7 +32,7 @@ __author__ = "Tom Graham"
 __email__ = "tom@sirwhite.com"
 
 
-from gmusicapi import Webclient, Musicmanager
+from gmusicapi import Webclient, Mobileclient, Musicmanager
 from gmusicapi.clients import OAUTH_FILEPATH
 import mutagen
 import json
@@ -42,6 +42,7 @@ import re
 import codecs
 from getpass import getpass
 from httplib import BadStatusLine, CannotSendRequest
+import sys
 
 MAX_UPLOAD_ATTEMPTS_PER_FILE = 3
 MAX_CONNECTION_ERRORS_BEFORE_QUIT = 5
@@ -49,10 +50,13 @@ STANDARD_SLEEP = 5
 MAX_SONGS_IN_PLAYLIST = 1000
 LOCAL_OAUTH_FILE = './oauth.cred'
 
+#sys.stdout = codecs.getwriter('cp1252')(sys.stdout)
+
 class MusicSync(object):
     def __init__(self, email=None, password=None):
         self.mm = Musicmanager()
         self.wc = Webclient()
+        self.mc = Mobileclient()
         if not email:
             email = raw_input("Email: ")
         if not password:
@@ -64,13 +68,18 @@ class MusicSync(object):
         self.logged_in = self.auth()
 
         print "Fetching playlists from Google..."
-        self.playlists = self.wc.get_all_playlist_ids(auto=False)
-        print "Got %d playlists." % len(self.playlists['user'])
+        self.playlists = self.mc.get_all_user_playlist_contents()
+        #self.playlists = self.mc.get_all_playlists()
+        #self.playlists = self.wc.get_all_playlist_ids(auto=False)
+        self.all_songs = self.mc.get_all_songs()
+        #print "Got %d playlists." % len(self.playlists['user'])
+        print "Got %d playlists containing %d songs." % (len(self.playlists), len(self.all_songs))
         print ""
 
 
     def auth(self):
-        self.logged_in = self.wc.login(self.email, self.password)
+        self.logged_in = self.mc.login(self.email, self.password)
+        #self.logged_in = self.wc.login(self.email, self.password)
         if not self.logged_in:
             print "Login failed..."
             exit()
@@ -91,21 +100,34 @@ class MusicSync(object):
         print ""
 
 
-    def sync_playlist(self, filename, remove_missing=False):
+    def sync_playlist(self, filename, remove_missing):
+    #def sync_playlist(self, filename, remove_missing=False):
         filename = self.get_platform_path(filename)
         os.chdir(os.path.dirname(filename))
         title = os.path.splitext(os.path.basename(filename))[0]
-        print "Synching playlist: %s" % filename
-        if title not in self.playlists['user']:
-            print "   didn't exist... creating..."
-            self.playlists['user'][title] = [self.wc.create_playlist(title)]
+        print "Syncing playlist: %s" % filename
+        #if title not in self.playlists['user']:
+            #print "   didn't exist... creating..."
+            #self.playlists['user'][title] = [self.wc.create_playlist(title)]
         print ""
 
-        plid = self.playlists['user'][title][0]
-        goog_songs = self.wc.get_playlist_songs(plid)
+        plid = ""
+
+        for pl in self.playlists:
+            if pl['name'] == title:
+                plid = pl['id']
+                goog_songs = pl['tracks']
+
+        if plid == "":
+            print "   didn't exist... creating..."
+            plid = self.mc.create_playlist(self, title)
+
+        #plid = self.playlists['user'][title][0]
+        #goog_songs = self.wc.get_playlist_songs(plid)
         print "%d songs already in Google Music playlist" % len(goog_songs)
         pc_songs = self.get_files_from_playlist(filename)
         print "%d songs in local playlist" % len(pc_songs)
+        print ""
 
         # Sanity check max 1000 songs per playlist
         if len(pc_songs) > MAX_SONGS_IN_PLAYLIST:
@@ -120,12 +142,15 @@ class MusicSync(object):
         fatal_count = 0
 
         for fn in pc_songs:
-            if self.file_already_in_list(fn, goog_songs):
+            if self.file_already_in_list(fn, goog_songs, self.all_songs):
                 existing_files += 1
                 continue
             print ""
-            print "Adding: %s" % os.path.basename(fn)
-            online = self.find_song(fn)
+            print "Adding: %s" % os.path.basename(fn).encode('cp1252')
+            #print "Adding: %s" % os.path.basename(fn)
+            #online = False
+            online = self.find_song(fn, goog_songs, self.all_songs)
+            #online = self.find_song(fn)
             song_id = None
             if online:
                 song_id = online['id']
@@ -149,6 +174,7 @@ class MusicSync(object):
                         print "Connection Error -- Reattempting login"
                         fatal_count += 1
                         self.wc.logout()
+                        self.mc.logout()
                         self.mm.logout()
                         result = []
                         time.sleep(STANDARD_SLEEP)
@@ -165,23 +191,28 @@ class MusicSync(object):
                     print "   upload complete [%s]" % song_id
                 except:
                     print "      upload failed - skipping"
+                    tag = self.get_id3_tag(fn)
+                    print "      failed song:\t%s\t%s\t%s" % (tag['title'].encode('cp1252'), tag['artist'].encode('cp1252'), tag['album'].encode('cp1252'))
 
             if not song_id:
                 failed_files += 1
                 continue
 
-            added = self.wc.add_songs_to_playlist(plid, song_id)
+            added = self.mc.add_songs_to_playlist(plid, song_id)
             time.sleep(.3) # Don't spam the server too fast...
             print "   done adding to playlist"
             added_files += 1
 
         if remove_missing:
-            for s in goog_songs:
-                print ""
-                print "Removing: %s" % s['title']
-                self.wc.remove_songs_from_playlist(plid, s.id)
-                time.sleep(.3) # Don't spam the server too fast...
-                removed_files += 1
+            for g in goog_songs:
+                for s in self.all_songs:
+                    if g['trackId'] == s['id']:
+                        print ""
+                        print "Removing: %s" % s['title'].encode('cp1252')
+                        self.mc.remove_entries_from_playlist(g['id'])
+                        #self.wc.remove_songs_from_playlist(plid, s.id)
+                        time.sleep(.3) # Don't spam the server too fast...
+                        removed_files += 1
 
         print ""
         print "---"
@@ -193,7 +224,8 @@ class MusicSync(object):
 
     def get_files_from_playlist(self, filename):
         files = []
-        f = codecs.open(filename, encoding='utf-8')
+        f = codecs.open(filename, encoding='cp1252')
+        #f = codecs.open(filename, encoding='utf-8')
         for line in f:
             line = line.rstrip().replace(u'\ufeff',u'')
             if line == "" or line[0] == "#":
@@ -206,13 +238,17 @@ class MusicSync(object):
         f.close()
         return files
 
-    def file_already_in_list(self, filename, goog_songs):
+    def file_already_in_list(self, filename, goog_songs, all_songs):
         tag = self.get_id3_tag(filename)
+        print "Searching for\t%s\t%s\t%s" % (tag['title'].encode('cp1252'), tag['artist'].encode('cp1252'), tag['album'].encode('cp1252'))
         i = 0
         while i < len(goog_songs):
-            if self.tag_compare(goog_songs[i], tag):
-                goog_songs.pop(i)
-                return True
+            for s in all_songs:
+                if goog_songs[i]['trackId'] == s['id']:
+                    if self.tag_compare(s, tag):
+                        print "Found match\t%s\t%s\t%s" % (s['title'].encode('cp1252'), s['artist'].encode('cp1252'), s['album'].encode('cp1252'))
+                        goog_songs.pop(i)
+                        return True
             i += 1
         return False
 
@@ -239,29 +275,43 @@ class MusicSync(object):
         r['album'] = data['album'][0] if 'album' in data else ''
         return r
 
-    def find_song(self, filename):
+    def find_song(self, filename, goog_songs, all_songs):
         tag = self.get_id3_tag(filename)
-        results = self.wc.search(tag['title'])
-        # NOTE - dianostic print here to check results if you're creating duplicates
+        print "Searching for\t%s\t%s\t%s" % (tag['title'].encode('cp1252'), tag['artist'].encode('cp1252'), tag['album'].encode('cp1252'))
+        #results = self.wc.search(tag['title'])
+        # NOTE - diagnostic print here to check results if you're creating duplicates
         #print results['song_hits']
-        #print "%s ][ %s ][ %s ][ %s" % (tag['title'], tag['artist'], tag['album'], tag['track'])
-        for r in results['song_hits']:
-            if self.tag_compare(r, tag):
+        #for r in goog_songs:
+        #for r in results['song_hits']:
+        for s in all_songs:
+            #if r['trackId'] == s['id']:
+            if self.tag_compare(s, tag):
                 # TODO: add rough time check to make sure its "close"
-                return r
+                print "Found match\t%s\t%s\t%s" % (s['title'].encode('cp1252'), s['artist'].encode('cp1252'), s['album'].encode('cp1252'))
+                return s
         return None
 
     def tag_compare(self, g_song, tag):
         # If a google result has no track, google doesn't return a field for it
+        if 'title' not in g_song:
+            g_song['title'] = ""
+        if 'artist' not in g_song:
+            g_song['artist'] = ""
+        if 'album' not in g_song:
+            g_song['album'] = ""
         if 'track' not in g_song:
             g_song['track'] = 0
+        if (g_song['title'].lower() == tag['title'].lower() and g_song['artist'].lower() == tag['artist'].lower()) or\
+           (g_song['album'].lower() == tag['album'].lower() and g_song['title'].lower() == tag['title'].lower()) or\
+           (g_song['artist'].lower() == tag['artist'].lower() and g_song['album'].lower() == tag['album'].lower() and g_song['track'] == tag['track']):
+            print "Partial match\t%s\t%s\t%s" % (g_song['title'].encode('cp1252'), g_song['artist'].encode('cp1252'), g_song['album'].encode('cp1252'))
         return g_song['title'].lower() == tag['title'].lower() and\
                g_song['artist'].lower() == tag['artist'].lower() and\
-               g_song['album'].lower() == tag['album'].lower() and\
-               g_song['track'] == tag['track']
+               g_song['album'].lower() == tag['album'].lower() #and\
+               #g_song['track'] == tag['track']
 
     def delete_song(self, sid):
-        self.wc.delete_songs(sid)
+        self.mc.delete_songs(sid)
         print "Deleted song by id [%s]" % sid
 
     def get_platform_path(self, full_path):
